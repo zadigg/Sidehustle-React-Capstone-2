ffd47fae44bbaf7a9297e46424edbb0adfb6d048
zadigg <62830716+zadigg@users.noreply.github.com>
2022-05-12 10:59:25 +0300

    Final Design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import Login from "./components/Login";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
--- src/components/DashBoardHeader.js
+++ src/components/DashBoardHeader.js
@@ -2,6 +2,7 @@ import React from "react";
 import { MenuIcon } from "@heroicons/react/solid";
 import "../styles/Header.css";
 import { useStateContext } from "../context/ToggleContext";
+import { Link } from "react-router-dom";
 
 const DashBoardHeader = () => {
   const { collapse, toggle } = useStateContext();
@@ -13,7 +14,9 @@ const DashBoardHeader = () => {
       </div>
       {!collapse ? <div className="SignInBtn">Sidehustle</div> : null}
 
-      <div className="SignInBtn">Sign Out</div>
+      <Link style={{ textDecoration: "none" }} to="/">
+        <div className="SignInBtn">Sign Out</div>
+      </Link>
     </div>
   );
 };
